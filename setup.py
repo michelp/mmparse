bdb5e0f4001b44ce7e2da6e1899c73cf462e24ea
Michel Pelletier <pelletier.michel@gmail.com>
2021-08-01 00:26:09 -0400

    array format.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='mmparse',
-    version='0.5',
+    version='0.6',
     description='Parse Matrix Market Files',
     author='Michel Pelletier',
     author_email='michel@graphegon.com',
